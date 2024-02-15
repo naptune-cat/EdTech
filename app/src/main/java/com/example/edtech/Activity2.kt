ede6e0da1b9092739f8c20ef8763fc9498a40d97
zoya <mkhanzoya8866@gmail.com>
2024-02-15 12:19:04 +0530

    Working made

--- app/src/main/java/com/example/edtech/Activity2.kt
+++ app/src/main/java/com/example/edtech/Activity2.kt
@@ -6,6 +6,7 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 
 class Activity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +18,36 @@ class Activity2 : AppCompatActivity() {
             intent.setData(Uri.parse("tel:123456789"))
             startActivity(intent)
         }
+        val kotlinImg = findViewById<ImageView>(R.id.kotlin)
+        kotlinImg.setOnClickListener{
+            intent = Intent(applicationContext,kotlinActivity::class.java)
+            startActivity(intent)
+        }
+        val cppImg = findViewById<ImageView>(R.id.cpp)
+        cppImg.setOnClickListener{
+            intent = Intent(applicationContext,cppActivity::class.java)
+            startActivity(intent)
+        }
+        val cssImg = findViewById<ImageView>(R.id.css)
+        cssImg.setOnClickListener{
+            intent = Intent(applicationContext,cssActivity::class.java)
+            startActivity(intent)
+        }
+        val htmlImg = findViewById<ImageView>(R.id.html)
+        htmlImg.setOnClickListener{
+            intent = Intent(applicationContext,htmlActivity::class.java)
+            startActivity(intent)
+        }
+        val tableauImg = findViewById<ImageView>(R.id.tableau)
+        tableauImg.setOnClickListener{
+            intent = Intent(applicationContext,tableauActivity::class.java)
+            startActivity(intent)
+        }
+        val sqlImg = findViewById<ImageView>(R.id.sql)
+        sqlImg.setOnClickListener{ 
+            intent = Intent(applicationContext,sqlActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
